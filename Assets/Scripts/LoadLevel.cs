8fee13807313c1f2370f0475b2b4d34f163aa1cd
AzureAD\LEOPOLDOPIATTIFADDA <leopoldo.piatti@enti.cat>
2023-11-22 18:18:42 +0100

    no message

---
+++ Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+    //public GameObject objectToActivate;
+    //public GameObject objectToDeactivate;
+
+    public Transform newPosition;
+
+    public string sceneName;
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void Salir()
+    {
+        Debug.Log("He Salido");
+        Application.Quit();
+    }
+    public void setactive(GameObject canvasactive)
+    {
+        canvasactive.SetActive(true);
+    }
+
+    public void setdesactive(GameObject canvasdesactive)
+    {
+        canvasdesactive.SetActive(false);
+    }
+    public void PauseGame()
+    {
+        Time.timeScale = 0f; // Set the time scale to 0 to pause the game
+    }
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f; // Set the time scale back to 1 to resume the game
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //objectToActivate.SetActive(true);
+            //objectToDeactivate.SetActive(false);
+            SceneManager.LoadScene(sceneName);
+            // Cambiar la posición del jugador en los ejes X e Y
+            other.transform.SetPositionAndRotation(newPosition.position, newPosition.rotation);
+        }
+
+    }
+}
