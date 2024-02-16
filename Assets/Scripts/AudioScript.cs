fdcdc73a4c7706a05e0fd25348416bb0681eb588
LeopoldoPiatti <leopoldo.piatti@enti.cat>
2024-02-16 16:52:00 +0100

    Audio

--- Assets/Scripts/AudioScript.cs
+++ Assets/Scripts/AudioScript.cs
@@ -2,22 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioScript : MonoBehaviour
 {
-
     public AudioMixer masterMixer;
+    public Slider masterSlider;
+    public Slider musicSlider;
+    public Slider sfxSlider;
+
+    // Volumen máximo almacenado para cada categoría
+    private float maxMasterVolume;
+    private float maxMusicVolume;
+    private float maxSFXVolume;
+
+    void Start()
+    {
+        // Obtener los valores actuales de los volúmenes y almacenarlos como el volumen máximo
+        masterMixer.GetFloat("MasterVolume", out maxMasterVolume);
+        masterMixer.GetFloat("MusicVolume", out maxMusicVolume);
+        masterMixer.GetFloat("SFXVolume", out maxSFXVolume);
+
+        // Configurar los valores máximos de los sliders
+        masterSlider.maxValue = maxMasterVolume;
+        musicSlider.maxValue = maxMusicVolume;
+        sfxSlider.maxValue = maxSFXVolume;
+    }
 
     public void SetSoundMaster(float soundLevel)
-    {        
+    {
         masterMixer.SetFloat("MasterVolume", soundLevel);
     }
+
     public void SetSoundMusic(float soundLevel)
     {
-        masterMixer.SetFloat("MusicVolume", soundLevel);        
+        masterMixer.SetFloat("MusicVolume", soundLevel);
     }
+
     public void SetSoundSFX(float soundLevel)
     {
-        masterMixer.SetFloat("SFXVolume", soundLevel);        
+        masterMixer.SetFloat("SFXVolume", soundLevel);
     }
 }
